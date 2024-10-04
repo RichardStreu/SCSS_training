5d788d1fc2c0a1c622b256b4cf54e0e5a520ccf5
Richard Streu <rstreu13@icloud.com>
2024-10-04 13:25:47 +0200

    added eample for global variables

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ printName(userMarly); // call the function and give her the used parameters (in 
 import { printAge as showDogsAge } from "./script/user.js"; // import the function. with the keyword "as" it's possible to rename the function by import.
 
 showDogsAge(userMarly); // now you can call the function by its new name and giving it the parameters needed.
+
+import { myExampleFuntion } from "./script/myFunctions"; // function hast to be declared in a file
+// ---> give the function to an HTML element like "onclick"
+
+window.myExampleFunction = myExampleFunction; // with this you can assign the declared funtion to the DOM
+
+// #########################################################################
+
+export let globalVariable = "Example"; // declare an global variable an export it, so it is been readable from any other file.
+// to overwrite the global variable, you have to declare a function to do this like the following:
+
+export function overwriteGlobalVariable(overwrite) {
+  globalVariable = overwrite;
+}
+// now you can import this function to every file and overwrite it from everywhere.
